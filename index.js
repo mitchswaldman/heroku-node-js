0915b864f661537ff7f453973a40fe5c8282e2a5
Ubuntu <ubuntu@ip-172-31-44-143.us-west-2.compute.internal>
2016-03-13 23:16:32 +0000

    Part 1b. Have node use readFile to return index page.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var express = require('express')
-var app = express()
+var http = require('http');
+var fs = require('fs');
+var requestListener = function(req, res){
+    fs.readFile('/home/ubuntu/fundstarter/index.html', 'utf8', function(err, data){
+	if(err){
+	    res.writeHead(500);
+	    res.end('No file found.');
+	}
+	res.writeHead(200);
+	res.end(data);
+    });
+}
 
-/* serves all the static files*/
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-/*serves main page*/
-app.get('/', function(request, response) {
-response.sendfile('index.html') 
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var server = http.createServer(requestListener);
+server.listen(8080);
