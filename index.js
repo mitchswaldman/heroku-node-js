91ef59b5097313f6cf410306f0614bc37e676e9c
Ubuntu <ubuntu@ip-172-31-44-143.us-west-2.compute.internal>
2016-03-15 21:00:47 +0000

    Part 2. Use node' fs read, open methods to accomplish reading of files.

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 var http = require('http');
 var fs = require('fs');
 var pathToIndex = __dirname + '/index.html';
-var buf = fs.readFileSync(pathToIndex, 'utf8');
+
 var requestListener = function(req, res){
-    res.writeHead(200, {"Content-type":"text/html"});
-    res.end(buf);
+   
+    fs.open(pathToIndex,'r', function(err, fd){
+	if(err){
+	    
+	}
+	fs.fstat(fd, function(err, stats){
+	    if(err){
+	    }
+	    var bufferSize = stats.size,
+	        offset = 0,
+                length = stats.size,
+	        buffer = new Buffer(bufferSize);
+	    fs.read(fd, buffer, offset, length, 0, function(err, bytesRead, buff){
+		if(err){
+		}
+		res.end(buff);
+	    });
+	});
+	
+    });
 }
 
 var port = process.env.PORT || 8080;
